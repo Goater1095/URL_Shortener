c3aa0b350f6383d2bfc8465661f3834a25bde3bd
goater <goater1095@gmail.com>
2020-10-29 10:36:01 +0800

    feat: add mongodb connection setting

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const exhbs = require('express-handlebars')
 const bodyParser = express.urlencoded({ extended: true })
+const mongoose = require('mongoose')
 
 const app = express()
 const port = 3000
 
 app.use(bodyParser)
+
+//set mongoDB
+mongoose.connect(`mongodb://localhost/url-list`, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = mongoose.connection
+db.on('error', () => {
+  console.log('Mongodb Error')
+})
+db.once('open', () => {
+  console.log('Mongodb Connected!')
+})
 
 //set engine
 app.engine('hbs', exhbs({ defaultLayout: 'main', extname: '.hbs' }))
