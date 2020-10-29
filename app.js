543218ebbf30aca90530a412a4fa2432a673ad64
goater <goater1095@gmail.com>
2020-10-29 10:23:18 +0800

    feat: add handlevars & index page

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-const express = require('express');
-const exhbs = require('express-handlebars');
+const express = require('express')
+const exhbs = require('express-handlebars')
+const bodyParser = express.urlencoded({ extended: true })
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
+
+app.use(bodyParser)
+
+//set engine
+app.engine('hbs', exhbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 //set routes
 app.get('/', (req, res) => {
-  res.send('Server start!');
-});
+  res.render('index')
+})
 
+app.post('/', (req, res) => {
+  console.log(req.body)
+  console.log('Success!')
+})
 //start server
 app.listen(port, () => {
-  console.log(`This server is start on http://localhost:${port}`);
-});
+  console.log(`This server is start on http://localhost:${port}`)
+})
